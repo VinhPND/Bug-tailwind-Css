5e7f852dd09aa1fd3aa0a8589170455d1d0e45b6
VinhPND <nguyenvinh05062002@gmail.com>
2024-12-23 19:22:26 +0700

    demo-js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,21 +7,12 @@ const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <div style={{ textAlign: "center", minHeight: "100vh", position: "relative" }}>
-      <h1>Welcome to Home Page</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen relative text-center">
+      <h1 className="text-2xl font-bold mb-4">Welcome to Home Page</h1>
       <Button onClick={() => navigate("/product")} text="Go to Product Page" />
       <Footer />
     </div>
   );
 };
-// đây chú Dinh thấy ở dòng 12, mình gọi component <Button thấy hông, 
-// giờ đơn giản lắm, 
-//có phải đặt tên cho nso rùi gọi hong
-// đặt tên như là "go tô Home page " hở chú D
-//kiểu Button1
-// à ko, ko cần đâu nè
-// nhìn chú V làm mẫu 1 lần nghen
-// đầu tiên copy dòng 12 =))
-// quay về Product page
 
 export default Home;
